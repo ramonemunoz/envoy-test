f41af9071be66634112c6e9d2bd11fc5dbc02794
Ramon Munoz <ramonmunoz1027@gmail.com>
2020-08-09 17:56:43 -0700

    removed surronuding fragment from index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,82 +121,80 @@ export default () => {
   `)
 
   return (
-    <>
-      <Layout>
-        <Hero {...bio} />
-        <div className="homepage-container">
-          <h2 className="text-center">
-            Contrary to popular belief, he knows exactly what he’s doing.
-          </h2>
-          <div className="content-container">
-            <Image
-              className="ironman-drawing"
-              fluid={ironmanapi.sharp.fluid}
-              alt="Iron Man"
-            />
-            <div className="about">
-              <div className="information-section">
-                <Image
-                  className="icon order-one"
-                  fluid={geniusicon.sharp.fluid}
-                  alt="Iron Man"
-                  imgStyle={{ objectFit: "contain" }}
-                />
-                <h3>Super-Genius Intelligence</h3>
-                <p>
-                  Tony Stark is far more than a mechanical engineering prodigy
-                  who graduated from the Massachusetts Institute of Technology
-                  with honors at the age of 17.
-                </p>
-              </div>
-              <div className="information-section order-two">
-                <Image
-                  className="icon order-two"
-                  fluid={engineericon.sharp.fluid}
-                  alt="Iron Man"
-                  imgStyle={{ objectFit: "contain" }}
-                />
-                <h3>Master Engineer</h3>
-                <p>
-                  He is an excellent engineer and mechanic capable of fixing
-                  almost any, if not all machinery.
-                </p>
-              </div>
-              <div className="information-section order-three">
-                <Image
-                  className="icon"
-                  fluid={businessmanicon.sharp.fluid}
-                  imgStyle={{ objectFit: "contain" }}
-                  alt="Iron Man"
-                />
-                <h3>Master Businessman</h3>
-                <p>
-                  Stark is extremely well-respected in the business world, able
-                  to command people's attentions when he speaks on economic
-                  matters. He has built up several multi-million dollar
-                  companies from virtually nothing.
-                </p>
-              </div>
-              <div className="information-section order-four">
-                <Image
-                  className="icon"
-                  fluid={tacticalicon.sharp.fluid}
-                  imgStyle={{ objectFit: "contain" }}
-                  alt="Iron Man"
-                />
-                <h3>Expert Tactician</h3>
-                <p>
-                  He is a brilliant tactician capable of quickly formulating
-                  battle strategies and new plans if the situation changes, like
-                  being able to elaborate complex plans in order to defeat
-                  different enemies.
-                </p>
-              </div>
+    <Layout>
+      <Hero {...bio} />
+      <div className="homepage-container">
+        <h2 className="text-center">
+          Contrary to popular belief, he knows exactly what he’s doing.
+        </h2>
+        <div className="content-container">
+          <Image
+            className="ironman-drawing"
+            fluid={ironmanapi.sharp.fluid}
+            alt="Iron Man"
+          />
+          <div className="about">
+            <div className="information-section">
+              <Image
+                className="icon order-one"
+                fluid={geniusicon.sharp.fluid}
+                alt="Iron Man"
+                imgStyle={{ objectFit: "contain" }}
+              />
+              <h3>Super-Genius Intelligence</h3>
+              <p>
+                Tony Stark is far more than a mechanical engineering prodigy who
+                graduated from the Massachusetts Institute of Technology with
+                honors at the age of 17.
+              </p>
+            </div>
+            <div className="information-section order-two">
+              <Image
+                className="icon order-two"
+                fluid={engineericon.sharp.fluid}
+                alt="Iron Man"
+                imgStyle={{ objectFit: "contain" }}
+              />
+              <h3>Master Engineer</h3>
+              <p>
+                He is an excellent engineer and mechanic capable of fixing
+                almost any, if not all machinery.
+              </p>
+            </div>
+            <div className="information-section order-three">
+              <Image
+                className="icon"
+                fluid={businessmanicon.sharp.fluid}
+                imgStyle={{ objectFit: "contain" }}
+                alt="Iron Man"
+              />
+              <h3>Master Businessman</h3>
+              <p>
+                Stark is extremely well-respected in the business world, able to
+                command people's attentions when he speaks on economic matters.
+                He has built up several multi-million dollar companies from
+                virtually nothing.
+              </p>
+            </div>
+            <div className="information-section order-four">
+              <Image
+                className="icon"
+                fluid={tacticalicon.sharp.fluid}
+                imgStyle={{ objectFit: "contain" }}
+                alt="Iron Man"
+              />
+              <h3>Expert Tactician</h3>
+              <p>
+                He is a brilliant tactician capable of quickly formulating
+                battle strategies and new plans if the situation changes, like
+                being able to elaborate complex plans in order to defeat
+                different enemies.
+              </p>
             </div>
           </div>
         </div>
-        <Slider {...comics} />
-      </Layout>
-    </>
+      </div>
+      <Slider {...comics} />
+    </Layout>
   )
 }
